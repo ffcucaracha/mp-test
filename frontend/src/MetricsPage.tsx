7bb04a8ecbcee70d21142238837507ac1a7a2199
ffcucaracha <32615933+ffcucaracha@users.noreply.github.com>
2026-09-07 01:22:09 +0600

    Load metrics and neighbors styles

--- frontend/src/MetricsPage.tsx
+++ frontend/src/MetricsPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 
 import { api } from './api'
+import './analytics-neighbors.css'
 import type { InternalMetrics } from './types'
 
 export function MetricsPage() {
